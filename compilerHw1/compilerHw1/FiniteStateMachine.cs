2b2a1871706040794141440b862f4e880844b5e7
aldy120 <aldy12345@gmail.com>
2014-10-29 00:49:15 +0800

    改一下表

---
+++ compilerHw1/compilerHw1/FiniteStateMachine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace compilerHw1
+{
+    class FiniteStateMachine
+    {
+        private static int[][] stateFunctionTable;
+        public static bool isAccept(string theText)
+        {
+            int tempState=1;
+            int theSymbolNum;
+            int textLength=theText.Length;
+            //char[] theTextChar = new char[textLength];
+            
+            for (int i = 0; i < textLength; i++)
+            {
+                if (theText[i] == ' ')
+                    continue;
+
+                theSymbolNum = classify(theText[i]);
+
+                tempState = stateFunctionTable[tempState][theSymbolNum];
+            }
+            if (tempState == 2 || tempState == 4 || tempState == 7)
+                return true;
+            return false;
+            
+        }
+        private static int classify(char c)
+        {
+            if (char.IsDigit(c))
+                return 0;
+            if (c == '.')
+                return 1;
+            if (c == 'E')
+                return 2;
+            if (c == '+' || c == '-')
+                return 3;            
+            return 4;
+        }
+        
+        public static void buildStateFunctionTable()
+        {
+            /*
+             *          0 1 2 3
+             * state    D . e +-else
+             *     0    0 0 0 0 0
+             *     1    2 0 0 0 0
+             *     2    2 3 5 0 0
+             *     3    4 0 0 0 0
+             *     4    4 5 0 0 0
+             *     5    7 0 0 6 0
+             *     6    7 0 0 0 0
+             *     7    7 0 0 0 0
+             */                 
+            stateFunctionTable=new int[8][];
+            stateFunctionTable[0]=new int[5]{0,0,0,0,0};
+            stateFunctionTable[1]=new int[5]{2,0,0,0,0};
+            stateFunctionTable[2]=new int[5]{2,3,5,0,0};
+            stateFunctionTable[3]=new int[5]{4,0,0,0,0};
+            stateFunctionTable[4]=new int[5]{4,5,0,0,0};
+            stateFunctionTable[5]=new int[5]{7,0,0,6,0};
+            stateFunctionTable[6]=new int[5]{7,0,0,0,0};
+            stateFunctionTable[7]=new int[5]{7,0,0,0,0};
+
+
+        }
+
+    }
+}
---
+++ compilerHw1/compilerHw1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace compilerHw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "來";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 47);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "Form1";
+            this.Text = "編譯器作業一";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
